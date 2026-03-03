4f5af3eb292a286d5564fb7b0118cf5e7b6e44ad
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2026-03-03 23:09:19 +0600

    add all authentication code

--- src/proxy.js
+++ src/proxy.js
@@ -1,17 +1,10 @@
 import { withAuth } from "next-auth/middleware";
 
-// Instead of the shorthand export, we use withAuth to explicitly 
-// export the middleware function
-export default withAuth(
-  function middleware(req) {
-    // This function can be left empty; withAuth handles the redirect logic
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token,
   },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
+});
 
 export const config = {
   matcher: ["/dashboard/:path*", "/admin/:path*"],
